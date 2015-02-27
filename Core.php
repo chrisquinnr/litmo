e520e539dd3ff799c397923236d8c98b4c47e74e
Chris Quinn <cquinn@theagencyonline.co.uk>
2015-02-27 21:05:33 +0000

    Now using document root for source path files

--- Core.php
+++ Core.php
@@ -17,17 +17,17 @@ class Core {
             'title' => 'Crimen Punsum',
             'real_title' => 'Crime and Punishment',
             'anchor' => 'crimenpunsum',
-            'source' => '/Users/chris/Sites/Litmo/source/pg2554.txt'),
+            'source' => '/source/pg2554.txt'),
         1 => array(
             'title' => 'Studmen Scarlem',
             'real_title' => 'A Study in Scarlet',
             'anchor' => 'studmenscarlem',
-            'source' => '/Users/chris/Sites/Litmo/source/pg244.txt'),
+            'source' => '/source/pg244.txt'),
         2 => array(
             'title' => 'Tailem Twosum',
             'real_title' => 'A Tail of Two Cities',
             'anchor' => 'tailemtwosum',
-            'source' => '/Users/chris/Sites/Litmo/source/pg98.txt')
+            'source' => '/source/pg98.txt')
     );
 
 
@@ -79,6 +79,8 @@ class Core {
      * @return array
      */
     private function parseFile($source_path){
+
+        $source_path = $_SERVER['DOCUMENT_ROOT'] . $source_path;
 
         $source = file_get_contents($source_path);
 
