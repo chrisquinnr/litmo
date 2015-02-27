a4c7f621829659a54b2e5232e853d536f3d067fd
Chris Quinn <cquinn@theagencyonline.co.uk>
2015-02-27 22:17:00 +0000

    Added wholely unecessary CC licence

--- index.php
+++ index.php
@@ -176,7 +176,7 @@ $site_name = sprintf(
 
 
 	<footer class="footer">
-		<p>&copy; Company 2014</p>
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Litmo</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://limitless-sea-7817.herokuapp.com" property="cc:attributionName" rel="cc:attributionURL">Chris Quinn</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
 	</footer>
 
 </div> <!-- /container -->
